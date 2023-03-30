79029397bf8cf72cac3279be0ace05ababe9a326
JAPHETHNYARANGA <nyaranga4@gmail.com>
2023-03-30 16:37:37 +0300

    Finished Creating dashboard ui. Next step is to fetch data from api to the dashboard ui

--- src/app/Dashboard/home/home.component.ts
+++ src/app/Dashboard/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Chart } from 'angular-highcharts';
+import { chart } from 'highcharts';
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +12,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+
+
+  chart = new Chart({
+    chart: {
+      type: 'pie'
+    },
+    title: {
+      text: 'Stats'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        name: ['stats'],
+        data: [
+          { name: 'Completed', y: 1 },
+          { name: 'ON going', y: 2 },
+          { name: 'Started', y: 3 }
+        ]
+      } as any
+    ]
+  });
+
+  // add point to chart serie
+  add() {
+    this.chart.addPoint(Math.floor(Math.random() * 10));
+  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChartModule } from 'angular-highcharts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { HomeComponent } from './Dashboard/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ChartModule
    
   ],
   providers: [],
